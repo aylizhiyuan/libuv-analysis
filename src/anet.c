7c2d18c08376ac1a13676251ba282389b59e8bd9
lizhiyuan <148334733@qq.com>
2021-01-20 18:00:12 +0800

    今天至少弄明白了TCP的延迟确认机制...

--- src/anet.c
+++ src/anet.c
@@ -2,7 +2,7 @@
  * @Author: lizhiyuan
  * @Date: 2021-01-07 15:11:35
  * @LastEditors: lizhiyuan
- * @LastEditTime: 2021-01-20 14:31:47
+ * @LastEditTime: 2021-01-20 16:46:43
  */
 #include "fmacros.h"
 #include <sys/types.h>
@@ -60,6 +60,45 @@ int anetSetBlock(char *err,int fd,int non_block){
         return ANET_ERR;
     }
     return ANET_OK;
+}
+
+int anetNonBlock(char *err,int fd){
+    return anetSetBlock(err,fd,1); // 设置为非阻塞模式
+}
+int anetBlock(char *err,int fd){
+    return anetSetBlock(err,fd,0); // 设置为阻塞模式
+}
+
+int anetKeepAlive(char *err,int fd,int interval){
+    int val = 1; // 1为开启,0为缺省
+    // 一般是为了检测TCP连接的情况,导致TCP断联(网线故障/断电)
+    // 应用层可发送心跳包,自定义心跳包的消息头,一般由客户端主动发送
+    // 此处是TCP协议自带的保活功能
+    if(setsockopt(fd,SOL_SOCKET,SO_KEEPALIVE,&val,sizeof(val)) == -1){
+        anetSetError(err, "setsockopt SO_KEEPALIVE: %s", strerror(errno));
+        return ANET_ERR;
+    }
+    #ifdef __liunx__
+        val = interval;
+        if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPIDLE, &val, sizeof(val)) < 0) {
+            anetSetError(err, "setsockopt TCP_KEEPIDLE: %s\n", strerror(errno));
+            return ANET_ERR;
+        }
+        val = interval/3;
+        if (val == 0) val = 1;
+        if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPINTVL, &val, sizeof(val)) < 0) {
+            anetSetError(err, "setsockopt TCP_KEEPINTVL: %s\n", strerror(errno));
+            return ANET_ERR;
+        }
+        val = 3;
+        if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPCNT, &val, sizeof(val)) < 0) {
+            anetSetError(err, "setsockopt TCP_KEEPCNT: %s\n", strerror(errno));
+            return ANET_ERR;
+        }
+    #else
+        ((void) interval);
+    #endif
+        return ANET_OK;
 }
 
 
